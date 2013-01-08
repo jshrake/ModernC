9ba44fdc056f34713df59a42bc0768dc45076760
justin <justinshrake@gmail.com>
2013-01-07 17:13:03 -0800

    random alpha commit

--- Chapter9/rand_alpha.c
+++ Chapter9/rand_alpha.c
@@ -3,15 +3,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define N N
+#define N 10
 
 void generate_random_walk(char walk[N][N]);
 void print_array(char walk[N][N]);
 
 int main(void)
 {
-    
+    char walk[N][N] = {{0}};
+    generate_random_walk(walk);
+    print_array(walk);    
     return 0;
+}
+
+void print_array(char walk[N][N])
+{
+  for(int i = 0; i < N; ++i)
+  {
+    for(int j = 0; j < N; ++j)
+    {
+      if(walk[i][j] == 0)
+        printf("* ");
+      else
+        printf("%c ", walk[i][j]);
+    }
+    printf("\n");
+  }
 }
 
 void generate_random_walk(char walk[N][N])
@@ -23,9 +40,51 @@ void generate_random_walk(char walk[N][N])
     do{
         walk[x][y] = ch;
         ++ch;
-        if(walk[x+1][y] == 0 || walk[x-1][y] == 0 || walk[x][y+1] == 0 || walk[x][y-1] == 0)
-            legal_moves = false;
-        else
-
+        bool move_up = walk[x+1][y] == 0 && x+1 < N;
+        bool move_right = walk[x][y+1] == 0 && y+1 < N;
+        bool move_down = walk[x-1][y] == 0 && x > 0;
+        bool move_left = walk[x][y-1] == 0 && y > 0;
+        // Exit if there are no available moves
+        if (!move_up && !move_right && !move_down && !move_left)
+          return;
+        bool need_move = true;
+        while(need_move)
+        {
+          int next_move = rand() % 4;
+          switch(next_move)
+          {
+            case 0:
+              if(move_up)
+              {
+                x = x + 1;
+                need_move = false;
+              }
+              break;
+            case 1:
+              if(move_right)
+              {
+                y = y + 1;
+                need_move = false;
+              }
+              break;
+            case 2:
+              if(move_down)
+              {
+                x = x - 1;
+                need_move = false;
+              }
+              break;
+            case 3:
+              if(move_left)
+              {
+                y = y -1;
+                need_move = false;
+              }
+              break;
+            default:
+              printf("Nooooooooo!");
+              break;
+          }
+        } 
     }while(ch <= 'Z' && legal_moves);
 }
